40e8a46274ee7caa9e950d771c858b174ff25e36
Peswani Yukta <yuktapeswani12@gmail.com>
2020-03-23 19:55:17 +0530

    Repositories created

---
+++ src/main/java/com/cms/blogcms/repository/CommentRepository.java
@@ -0,0 +1,10 @@
+package com.cms.blogcms.repository;
+
+
+import com.cms.blogcms.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment,Long> {
+}
---
+++ src/main/java/com/cms/blogcms/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.cms.blogcms.repository;
+
+
+import com.cms.blogcms.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post,Long> {
+}
